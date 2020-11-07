9ab09fa32f1373673267a760a5ad5bbcf6c274df
abim_bola <bamgbeluabimbolainfo@gmail.com>
2020-11-07 10:37:03 +0000

    add index

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 //jshint esversion:6
 require('dotenv').config();
 const twitter = require('twitter-lite');
+const express = require('express');
 const config = require('./src/config/config/key');
 const client = new twitter(config);
+
+const app = express();
+
+  app.get("/", function (req, res){
+ res.send("bot is working");
+  });
 
 
  
